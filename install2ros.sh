e4bf8ced9fb153927901e4d79fd95fd135377f3a
Yuxuan Liu <uken.ryu.2@tier4.jp>
2024-04-05 11:35:45 +0900

    allow temp backup
    Signed-off-by: Yuxuan Liu <uken.ryu.2@tier4.jp>

--- install2ros.sh
+++ install2ros.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cat install2ros.sh | tail -n +4
+
+## Backup the scripts
+sudo cp /opt/ros/humble/lib/python3.10/site-packages/launch/actions/include_launch_description.py /opt/ros/humble/lib/python3.10/site-packages/launch/actions/include_launch_description.py.bak
+sudo cp /opt/ros/humble/lib/python3.10/site-packages/launch/launch_description.py /opt/ros/humble/lib/python3.10/site-packages/launch/launch_description.py.bak 
+
+## Install new scripts
 sudo cp launch/launch/actions/include_launch_description.py /opt/ros/humble/lib/python3.10/site-packages/launch/actions/include_launch_description.py
 sudo cp launch/launch/launch_description.py /opt/ros/humble/lib/python3.10/site-packages/launch/launch_description.py
 
